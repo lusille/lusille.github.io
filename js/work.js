da89853ed5d5888eb8bbb2f898126d34256392eb
lusille <styner36@naver.com>
2019-03-11 19:46:32 +0900

    portfolio

---
+++ js/work.js
@@ -0,0 +1,17 @@
+//work slider
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
